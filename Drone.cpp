93599c0d46ab9059542a14d60bd3ada50f79e7c0
Jed Borovik <mr.borovik@gmail.com>
2015-04-11 21:33:48 -0700

    Add all methods to Drone.h and Drone.cpp

--- Drone.cpp
+++ Drone.cpp
@@ -4,45 +4,49 @@
 #include "Drone.h"
 #include <avr/io.h>
 
+
+//
+// Constructors
+//
+
 Drone::Drone() {
   serialIO = SerialIO(serialPort0);
   callback = Callback();
-  avantRC = RC(&serialIO, &callback);
-  avantGPIO = GPIO(&serialIO, &callback);
+  rc = RC(&serialIO, &callback);
+  gpio = GPIO(&serialIO, &callback);
   responseHandler = ResponseHandler(&serialIO, &callback);
-  avantI2C = I2C(&serialIO, &callback);
-  avantPose = Pose(&serialIO, &callback);
-  avantAutoPilot = AutoPilot(&serialIO, &callback);
+  i2c = I2C(&serialIO, &callback);
+  pose = Pose(&serialIO, &callback);
+  autoPilot = AutoPilot(&serialIO, &callback);
 }
 
 Drone::Drone(SerialPort serialPort) {
   serialIO = SerialIO(serialPort);
   callback = Callback();
-  avantRC = RC(&serialIO, &callback);
-  avantGPIO = GPIO(&serialIO, &callback);
+  rc = RC(&serialIO, &callback);
+  gpio = GPIO(&serialIO, &callback);
   responseHandler = ResponseHandler(&serialIO, &callback);
-  avantI2C = I2C(&serialIO, &callback);
-  avantPose = Pose(&serialIO, &callback);
-  avantAutoPilot = AutoPilot(&serialIO, &callback);
+  i2c = I2C(&serialIO, &callback);
+  pose = Pose(&serialIO, &callback);
+  autoPilot = AutoPilot(&serialIO, &callback);
 }
+
 Drone::Drone(int txPin, int rxPin) {
   serialIO = SerialIO(txPin, rxPin);
   callback = Callback();
-  avantRC = RC(&serialIO, &callback);
-  avantGPIO = GPIO(&serialIO, &callback);
+  rc = RC(&serialIO, &callback);
+  gpio = GPIO(&serialIO, &callback);
   responseHandler = ResponseHandler(&serialIO, &callback);
-  avantI2C = I2C(&serialIO, &callback);
-  avantPose = Pose(&serialIO, &callback);
-  avantAutoPilot = AutoPilot(&serialIO, &callback);
+  i2c = I2C(&serialIO, &callback);
+  pose = Pose(&serialIO, &callback);
+  autoPilot = AutoPilot(&serialIO, &callback);
 }
 
-GPIO& Drone::gpio() {return avantGPIO;} 
-ResponseHandler& Drone::avantResponseHandler(){return responseHandler;}
-RC& Drone::rc() {return avantRC;}
-I2C& Drone::i2c() {return avantI2C;}
-Pose& Drone::pose() {return avantPose;}
-AutoPilot& Drone::autoPilot() {return avantAutoPilot;}
+//
+// Initialization methods
+//
 
+// TODO: figure out how to make this work in the constructor.
 void Drone::initialize() {
   serialIO.softwareSerial.begin(57600);
 }
@@ -58,3 +62,139 @@ void Drone::arm() {
   serialIO.sendPacket((int8_t)0, 2, 3);
   serialIO.sendPacket((int8_t)0, 2, 4);
 }
+
+
+void Drone::listen() {
+
+}
+
+// 
+// Pose Methods
+//
+
+void Drone::getGPSData()                              { pose.getGPSData(); }
+void Drone::getLongitude()                            { pose.getLongitude(); }
+void Drone::getLatitude()                             { pose.getLatitude(); }
+void Drone::getAltitude()                             { pose.getAltitude(); }
+void Drone::getSatellites()                           { pose.getSatellites(); }
+void Drone::getSpeed()                                { pose.getSpeed(); }
+void Drone::getOrientation()                          { pose.getOrientation(); }
+void Drone::setLongitudeCallback(void (*cb)(float))   { pose.setLongitudeCallback(cb); }
+void Drone::setLatitudeCallback(void (*cb)(float))    { pose.setLatitudeCallback(cb); }
+void Drone::setAltitudeCallback(void (*cb)(float))    { pose.setAltitudeCallback(cb); }
+void Drone::setSatelliteCallback(void (*cb)(byte))    { pose.setSatelliteCallback(cb); }
+void Drone::setSpeedCallback(void (*cb)(float))       { pose.setSpeedCallback(cb); }
+void Drone::setOrientationCallback(void (*cb)(float)) { pose.setOrientationCallback(cb); }
+
+//
+// TODO: rename RC
+// RC Methods
+//
+
+void Drone::setAileron(int8_t value)                { rc.setAileron(value); }
+void Drone::setElevator(int8_t value)               { rc.setElevator(value); }
+void Drone::setThrottle(int8_t value)               { rc.setThrottle(value); }
+void Drone::setRudder(int8_t value)                 { rc.setRudder(value); }
+void Drone::setFlightMode(int8_t value)             { rc.setFlightMode(value); }
+void Drone::getAileron()                            { rc.getAileron(); }
+void Drone::getElevator()                           { rc.getElevator(); }
+void Drone::getThrottle()                           { rc.getThrottle(); }
+void Drone::getRudder()                             { rc.getRudder(); }
+void Drone::getFlightMode()                         { rc.getFlightMode(); }
+void Drone::setAileronCallback(void (*cb)(byte))    { rc.setAileronCallback(cb); }
+void Drone::setFlightModeCallback(void (*cb)(byte)) { rc.setFlightModeCallback(cb); }
+void Drone::setThrottleCallback(void (*cb)(byte))   { rc.setThrottleCallback(cb); }
+void Drone::setRudderCallback(void (*cb)(byte))     { rc.setRudderCallback(cb); }
+void Drone::setElevatorCallback(void (*cb)(byte))   { rc.setElevatorCallback(cb); }
+void Drone::sendRTEA(uint8_t rudder, uint8_t throttle, uint8_t elevator, uint8_t aileron) {
+  // TODO: rename RTEA to something less cryptic
+  rc.sendRTEA(rudder, throttle, elevator, aileron);
+}
+
+//
+// GPIO Methods
+//
+
+void Drone::pinMode(uint8_t pin, int logicLevel)                  { gpio.pinMode(pin, logicLevel); }
+void Drone::digitalWrite(uint8_t pin, bool logicLevel)            { gpio.digitalWrite(pin, logicLevel); }
+void Drone::analogWrite(uint8_t pin, uint8_t value)               { gpio.analogWrite(pin, value); }
+void Drone::pulseIn(uint8_t pin)                                  { gpio.pulseIn(pin); }
+void Drone::digitalRead(uint8_t pin)                              { gpio.digitalRead(pin); }
+void Drone::analogRead(uint8_t pin)                               { gpio.analogRead(pin); }
+void Drone::setDigitalReadCallback(void (*cb)(byte), int pin)     { gpio.setDigitalReadCallback(cb); }
+void Drone::setPulseInCallback(void (*cb)(long), uint8_t pin)     { gpio.setPulseInCallback(cb); }
+void Drone::setAnalogReadCallback(void (*cb)(byte), uint8_t pin)  { gpio.setAnalogReadCallback(cb, pin); }
+void Drone::attachServo(uint8_t servoNumber, uint8_t pin)         { gpio.attachServo(servoNumber, pin); }
+void Drone::detachServo(uint8_t servoNumber)                      { gpio.detachServo(servoNumber); }
+void Drone::writeServo(uint8_t servoNumber, uint8_t data)         { gpio.writeServo(servoNumber, data); }
+
+//
+// I2C Methods
+//
+
+void Drone::deviceID(uint8_t id)              { i2c.deviceID(id); }
+void Drone::beginTransmission()               { i2c.beginTransmission(); }
+void Drone::endTransmission()                 { i2c.endTransmission(); }
+void Drone::write(uint8_t data)               { i2c.write(data); }
+void Drone::read()                            { i2c.read(data); }
+void Drone::wireRequest(uint8_t byteCount)    { i2c.writeRequest(byteCount); }
+void Drone::setReadCallback(void (*cb)(byte)) { i2c.setReadCallback(cb); }
+
+//
+// Autopilot Methods
+//
+
+void Drone::gpsExecute()                                      { autoPilot.gpsExecute(); }
+void Drone::compassExecute()                                  { autoPilot.compassExecute(); }
+void Drone::setYawError(float error)                          { autoPilot.setYawError(error); }
+void Drone::setThrottleError(float error)                     { autoPilot.setThrottleError(error); }
+void Drone::setElevatorError(float error)                     { autoPilot.setElevatorError(error); }
+void Drone::setAileronError(float error)                      { autoPilot.setAileronError(error); }
+void Drone::setWaypointLatitude(float latitude)               { autoPilot.setWaypointLatitude(latitude); }
+void Drone::setWaypointLongitude(float longitude)             { autoPilot.setWaypointLongitude(longitude); }
+void Drone::setWaypointAltitude(float altitude)               { autoPilot.setWaypointAltitude(altitude); }
+void Drone::setWaypointOrientation(float orientation)         { autoPilot.setWaypointOrientation(orientation); }
+void Drone::setYawKP(float kp)                                { autoPilot.setYawKP(kp); }
+void Drone::setYawKD(float kd)                                { autoPilot.setYawKD(kd); }
+void Drone::setYawKI(float ki)                                { autoPilot.setYawKI(ki); }
+void Drone::setThrottleKP(float kp)                           { autoPilot.setThrottleKP(kp); }
+void Drone::setThrottleKD(float kd)                           { autoPilot.setThrottleKD(kd); }
+void Drone::setThrottleKI(float ki)                           { autoPilot.setThrottleKI(ki); }
+void Drone::setElevatorKP(float kp)                           { autoPilot.setElevatorKP(kp); }
+void Drone::setElevatorKD(float kd)                           { autoPilot.setElevatorKD(kd); }
+void Drone::setElevatorKI(float ki)                           { autoPilot.setElevatorKI(ki); }
+void Drone::setAileronKP(float kp)                            { autoPilot.setAileronKP(kp); }
+void Drone::setAileronKD(float kd)                            { autoPilot.setAileronKD(kd); }
+void Drone::setAileronKI(float ki)                            { autoPilot.setAileronKI(ki); }
+void Drone::getWaypointLatitude()                             { autoPilot.getWaypointLatitude(); }
+void Drone::getWaypointLongitude()                            { autoPilot.getWaypointLongitude(); }
+void Drone::getWaypointAltitude()                             { autoPilot.getWaypointAltitude(); }
+void Drone::getWaypointOrientation()                          { autoPilot.getWaypointOrientation(); }
+void Drone::getYawKP()                                        { autoPilot.getYawKP(); }
+void Drone::getYawKD()                                        { autoPilot.getYawKD(); }
+void Drone::getYawKI()                                        { autoPilot.getYawKI(); }
+void Drone::getThrottleKP()                                   { autoPilot.getThrottleKP(); }
+void Drone::getThrottleKD()                                   { autoPilot.getThrottleKD(); }
+void Drone::getThrottleKI()                                   { autoPilot.getThrottleKI(); }
+void Drone::getElevatorKP()                                   { autoPilot.getElevatorKP(); }
+void Drone::getElevatorKD()                                   { autoPilot.getElevatorKD(); }
+void Drone::getElevatorKI()                                   { autoPilot.getElevatorKI(); }
+void Drone::getAileronKP()                                    { autoPilot.getAileronKP(); }
+void Drone::getAileronKD()                                    { autoPilot.getAileronKD(); }
+void Drone::getAileronKI()                                    { autoPilot.getAileronKI(); }
+void Drone::setWaypointLatitudeCallback(void (*cb)(float))    { autoPilot.setWaypointLatitudeCallback(cb); }
+void Drone::setWaypointLongitudeCallback(void (*cb)(float))   { autoPilot.setWaypointLongitudeCallback(cb); }
+void Drone::setWaypointAltitudeCallback(void (*cb)(float))    { autoPilot.setWaypointAltitudeCallback(cb); }
+void Drone::setWaypointOrientationCallback(void (*cb)(float)) { autoPilot.setWaypointOrientationCallback(cb); }
+void Drone::setYawKPCallback(void (*cb)(float))               { autoPilot.setYawKPCallback(cb); }
+void Drone::setYawKDCallback(void (*cb)(float))               { autoPilot.setYawKDCallback(cb); }
+void Drone::setYawKICallback(void (*cb)(float))               { autoPilot.setYawKICallback(cb); }
+void Drone::setThrottleKPCallback(void (*cb)(float))          { autoPilot.setThrottleKPCallback(cb); }
+void Drone::setThrottleKDCallback(void (*cb)(float))          { autoPilot.setThrottleKDCallback(cb); }
+void Drone::setThrottleKICallback(void (*cb)(float))          { autoPilot.setThrottleKICallback(cb); }
+void Drone::setElevatorKPCallback(void (*cb)(float))          { autoPilot.setElevatorKPCallback(cb); }
+void Drone::setElevatorKDCallback(void (*cb)(float))          { autoPilot.setElevatorKDCallback(cb); }
+void Drone::setElevatorKICallback(void (*cb)(float))          { autoPilot.setElevatorKICallback(cb); }
+void Drone::setAileronKPCallback(void (*cb)(float))           { autoPilot.setAileronKPCallback(cb); }
+void Drone::setAileronKDCallback(void (*cb)(float))           { autoPilot.setAileronKDCallback(cb); }
+void Drone::setAileronKICallback(void (*cb)(float))           { autoPilot.setAileronKICallback(cb); }
--- Drone.h
+++ Drone.h
@@ -31,31 +31,490 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "ResponseHandler.h"
 #include "AutoPilot.h"
 
-//\cond
 class Drone 
-//\endcond
 {
-    private:
-      RC avantRC;
-      SerialIO serialIO;
-      GPIO avantGPIO;
-      ResponseHandler responseHandler;
-      I2C avantI2C;
-      Callback callback;
-      Pose avantPose;
-      AutoPilot avantAutoPilot;
-    public:
-      Drone();
-      Drone(SerialPort serialPort);
-      Drone(int txPin, int rxPin);
-      RC& rc();
-      GPIO& gpio();
-      I2C& i2c();
-      Pose& pose();
-      ResponseHandler& avantResponseHandler();
-      AutoPilot& autoPilot();
-      void arm();
-      void initialize();
+private:
+  RC rc;
+  SerialIO serialIO;
+  GPIO gpio;
+  ResponseHandler responseHandler;
+  I2C i2c;
+  Callback callback;
+  Pose pose;
+  AutoPilot autoPilot;
+public:
+  Drone();
+  Drone(SerialPort serialPort);
+  Drone(int txPin, int rxPin);
+
+  void listen();
+  void arm();
+  void initialize();
+
+  /**
+       
+  This sends only one message to request all of the POSE data. 
+  The Extender will reply with 6 messages containing Long, Lat, number of Sat, Speed, Orientation, and Altitude
+       
+  */
+  void getGPSData();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the vehicles current Longitude
+  The response gets handled through the callback function longitudeCallbackCallback().
+       
+  */
+  void getLongitude();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the vehicles current Latitude.
+  The response gets handled through the callback function latitudeCallback().
+       
+  */
+  void getLatitude();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the vehicles current Altitude.
+  The response gets handled through the callback function altitudeCallback().
+       
+  */
+  void getAltitude();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the current number of satellites the vehicles
+  GPS is locked onto.
+  The response gets handled through the callback function satelliteCallback().
+  The more satellites you have the higher the accuracy is.  There needs to be a minimum of 4 satellites to get a lock.  
+       
+  */
+  void getSatellites();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the vehicles current Land Speed given by the GPS.
+  The response gets handled through the callback function speedCallback().
+       
+  */
+  void getSpeed();
+
+  /**
+       
+  Sends a request to the Extender asking for it to reply with the vehicles current Orientation given by the Compass.
+  The response gets handled through the callback function orientationCallback().
+       
+  */
+  void getOrientation();
+
+  /**
+       
+  Callback function which passes longitude information to the function that it is passed.
+       
+  */
+  void setLongitudeCallback(void (*function)(float));
+
+  /**
+       
+  Callback function which passes latitude information to the function that it is passed.
+       
+  */
+  void setLatitudeCallback(void (*function)(float));
+
+  /**
+       
+  Callback function which passes altitude information to the function that it is passed.
+       
+  */
+  void setAltitudeCallback(void (*function)(float));
+
+  /**
+       
+  Callback function which passes number of locked satellites to the function that it is passed.
+       
+  */
+  void setSatelliteCallback(void (*function)(byte));
+
+  /**
+       
+  Callback function which passes speed information to the function that it is passed.
+       
+  */
+  void setSpeedCallback(void (*function)(float));
+
+  /**
+       
+  Callback function which passes orientation information to the function that it is passed
+       
+  */
+  void setOrientationCallback(void (*function)(float));
+
+  /**
+
+  Sends a request to the extender to set the Aileron channel to a specific channel between -100 and 100.
+
+  @param value The value to set the Aileron channel to
+
+  */
+  void setAileron(int8_t value);
+
+  /**
+
+  Sends a request to the extender to set the Extender channel to a specific channel between -100 and 100.
+
+  @param value The value to set the Extender channel to
+
+  */
+  void setElevator(int8_t value);
+
+  /**
+
+  Sends a request to the extender to set the Throttle channel to a specific channel between -100 and 100.
+
+  @param value The value to set the Throttle channel to
+
+  */
+  void setThrottle(int8_t value);
+
+  /**
+
+  Sends a request to the extender to set the Rudder channel to a specific channel between -100 and 100.
+
+  @param value The value to set the Rudder channel to
+
+  */
+  void setRudder(int8_t value);
+
+  /**
+
+  Sends a request to the extender to set the Flight Mode channel to a specific channel between -100 and 100.
+
+  @param value The value to set the Flight Mode channel to
+
+  */
+  void setFlightMode(int8_t value);
+
+  /**
+
+  Sends a request to the extender to reply with message containing the current Aileron value.
+
+  */
+  void getAileron();
+
+  /**
+
+  Sends a request to the extender to reply with message containing the current Elevator value.
+
+  */
+  void getElevator();
+
+  /**
+
+  Sends a request to the extender to reply with message containing the current Throttle value.
+
+  */
+  void getThrottle();
+
+  /**
+
+  Sends a request to the extender to reply with message containing the current Rudder value.
+
+  */
+  void getRudder();
+
+  /**
+
+  Sends a request to the extender to reply with message containing the current Flight Mode value.
+
+  */
+  void getFlightMode();
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void setAileronCallback(void (*function)(byte));
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void setFlightModeCallback(void (*function)(byte));
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void setThrottleCallback(void (*function)(byte));
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void setRudderCallback(void (*function)(byte));
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void setElevatorCallback(void (*function)(byte));
+
+  /**
+
+  Execute funtion in a thread to not freez the drone.
+
+  */
+  void sendRTEA(uint8_t rudder, uint8_t throttle, uint8_t elevator, uint8_t aileron);
+  
+  /**
+   
+  Configures the specified pin on the App Extender to behave either as an input or an output.
+
+   
+  @param pin value expected 0 to 10
+  @param logicLevel expected 0 or 1
+   
+  */
+  void pinMode(uint8_t pin, int logicLevel);
+
+  /**
+
+  Write a HIGH or LOW value to a digital pin on the App Extender.
+
+  @param pin value expected 0 to 10
+  @param logicLevel expected 0 or 1
+
+  */
+  void digitalWrite(uint8_t pin, bool logicLevel);
+
+  /**
+
+  Writes an analog value (PWM wave) to the specified pin on the App Extender. 
+
+  @param pin sets pins 1-10 on the App Extender to the PWM value specified by value
+  @param value sets the PWM values duty cycle ranging from 0 and 255 
+
+  */
+  void analogWrite(uint8_t pin, uint8_t value);
+
+  /**
+   
+  Writes an a request to the Extender to check the duration of a pulse on the specified pin.
+
+  @param pin sets pins 1-10 on the App Extender to the PWM value specified by value 
+
+  */
+  void pulseIn(uint8_t pin);
+
+
+  /**
+
+  Sends a request to the App Extender to reply with the logical state of the specified pin.  
+
+  @param pin Selects which GPIO pin on the App Extender it should return.
+
+  */
+  void digitalRead(uint8_t pin);
+
+  /**
+   
+  Sends a request to the App Extender to reply with the analog value of the specified pin.  
+
+  @param pin Selects which analog pin on the App Extender it should return.
+
+  */
+  void analogRead(uint8_t pin);
+
+  /**
+
+  This callback function is executed everytime it gets digitalRead information.  It passes
+  this information to the function specified in the argument.
+
+  @param function function name of the function to pass received information to
+  @param pin the desired pin to sample logic level information from 
+
+  */
+  void setDigitalReadCallback(void (*function)(byte), int pin);
+
+  /**
+
+  This callback function is executed every time it gets pulseIn information.  It passes
+  this information to the function specified in the argument.
+   
+  @param function name of the function to pass received information to
+  @param pin the desired pin to sample pulseIn information from 
+   
+  */
+  void setPulseInCallback(void (*function)(long), uint8_t pin);
+
+  /**
+   
+  This callback function is executed every time it gets analogRead information.  It passes
+  this information to the function specified in the argument.
+   
+  @param function function name of the function to pass received information to
+  @param pin the desired pin to sample analog information from 
+   
+  */
+  void setAnalogReadCallback(void (*function)(byte), uint8_t pin);
+
+  /**
+
+  Associates a servo with a pin.
+
+  @param servoNumber which of the three servos to attach. Values should be 1-3.
+  @param pin the desired pin to associate with the servo.
+
+  */
+  void attachServo(uint8_t servoNumber, uint8_t pin);
+
+  /**
+
+  Clear a previously set assocation between the given servo and pin.
+
+  @param servoNumber the desired servo to detach. Values should be 1-3.
+
+  */
+  void detachServo(uint8_t servoNumber);
+
+  /**
+
+  Write data to a servo. This servo should already be attached via the `attachServo` method.
+
+  @param servoNumber the servo to send data too. Values should be 1-3.
+  @param data the data to send to the servo.
+
+  */
+  void writeServo(uint8_t servoNumber, uint8_t data);
+
+  /**
+       
+  This sets the address of the device that the App Extender sends data too.
+  This address will be continuously used by beginTransmission, write, read, and wireRequest
+  until it is reset to a different value.  
+      
+  @param ID range from 0 to 255
+      
+  */
+  void deviceID(uint8_t ID);
+
+  /**
+
+  Begin a transmission to the I2C slave device with the address specified by deviceID(ID)
+  Subsequently, queue bytes for transmission with the write() function and transmit them
+  by calling endTransmission().
+      
+  */
+  void beginTransmission(void);
+
+  /**
+
+  Ends a transmission to a slave device that was begun by beginTransmission() and transmits
+  the bytes that were queued by write().
+       
+  */
+  void endTransmission(void);
+
+  /**
+       
+  Queues bytes for transmission from the App Extender to a slave device, in between calls to 
+  beginTransmission() and endTransmission().  
+      
+  @param data Adds one byte of information to the queue
+      
+  */
+  void write(uint8_t data);
+
+  /**
+
+  Sends a request to the App Extender to read one byte of information from the I2C buffer and return 
+  the information to the arduino.  The relpies are handled by the readCallback() function.  
+     
+  */
+  void read(void);
+
+  /**
+
+  Sends a request to the App Extender to read multiple bytes of information from the I2C buffer and return 
+  the information to the arduino. The relpies are handled by the readCallback() function.  
+      
+  @param bytes Sets the number of bytes to read from the I2C buffer
+      
+  */
+  void wireRequest(uint8_t bytes);
+
+  /**
+       
+  This callback function is executed everytime it gets I2C information.  It passes
+  this information to the function specified in the argument.
+      
+  @param function name of the method
+
+  */
+  void readCallback(void (*function)(byte));
+
+
+  void gpsExecute();
+  void compassExecute();
+  void setYawError(float error);
+  void setThrottleError(float error);
+  void setElevatorError(float error);
+  void setAileronError(float error);
+  void setWaypointLatitude(float latitude);
+  void setWaypointLongitude(float longitude);
+  void setWaypointAltitude(float altitude);
+  void setWaypointOrientation(float orientation);
+  void setYawKP(float kp);
+  void setYawKD(float kd);
+  void setYawKI(float ki);
+  void setThrottleKP(float kp);
+  void setThrottleKD(float kd);
+  void setThrottleKI(float ki);
+  void setElevatorKP(float kp);
+  void setElevatorKD(float kd);
+  void setElevatorKI(float ki);
+  void setAileronKP(float kp);
+  void setAileronKD(float kd);
+  void setAileronKI(float ki);
+  void getWaypointLatitude();
+  void getWaypointLongitude();
+  void getWaypointAltitude();
+  void getWaypointOrientation();
+  void getYawKP();
+  void getYawKD();
+  void getYawKI();
+  void getThrottleKP();
+  void getThrottleKD();
+  void getThrottleKI();
+  void getElevatorKP();
+  void getElevatorKD();
+  void getElevatorKI();
+  void getAileronKP();
+  void getAileronKD();
+  void getAileronKI();
+  void setWaypointLatitudeCallback(void (*cb)(float));
+  void setWaypointLongitudeCallback(void (*cb)(float));
+  void setWaypointAltitudeCallback(void (*cb)(float));
+  void setWaypointOrientationCallback(void (*cb)(float));
+  void setYawKPCallback(void (*cb)(float));
+  void setYawKDCallback(void (*cb)(float));
+  void setYawKICallback(void (*cb)(float));
+  void setThrottleKPCallback(void (*cb)(float));
+  void setThrottleKDCallback(void (*cb)(float));
+  void setThrottleKICallback(void (*cb)(float));
+  void setElevatorKPCallback(void (*cb)(float));
+  void setElevatorKDCallback(void (*cb)(float));
+  void setElevatorKICallback(void (*cb)(float));
+  void setAileronKPCallback(void (*cb)(float));
+  void setAileronKDCallback(void (*cb)(float));
+  void setAileronKICallback(void (*cb)(float));
 };
 
 #endif /* defined __ArduinoSDK__Drone__ */
