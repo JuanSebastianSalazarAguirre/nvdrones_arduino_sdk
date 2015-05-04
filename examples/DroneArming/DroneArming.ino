00b0a4d2d5303c762a32ae4704d9b0929ce88aa2
Jed Borovik <mr.borovik@gmail.com>
2015-05-03 22:12:08 -0700

    Update DroneArming.ino

--- examples/DroneArming/DroneArming.ino
+++ examples/DroneArming/DroneArming.ino
@@ -1,21 +1,14 @@
 #include <Drone.h>
 
-Drone drone(serialPort1); //create an instance of Avant
-                  //0 means Serial, 1 is Serial1, 2 is Serial2, 3 is Serial3
-                  //If the Xbee is connected to software serial ports
-                  //Specify the RX, TX port like myTrans(rx, tx)
+Drone drone(serialPort1);
 
 void setup() {
-  Serial.begin(57600);
-  delay(3000);             //turn the drone on and off every 5 seconds
-  Serial.println("Arming 3");
-  drone.arm();     //This will turn the drone on and off
+
 }
 
 void loop() {
-
+  // Turn the drone on and off every 3 seconds.
+  delay(3000);
+  Serial.println("Arming");
+  drone.arm();
 }
-
-
-
-
